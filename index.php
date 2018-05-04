88880079c4885ed5428be27dabe45ef305c8c356
mm909 <Bobar312@gmail.com>
2018-05-03 20:46:35 -0700

    Did it! SI text box working with divs
    used code from
    
    https://codepen.io/SzymonDziewonski/pen/RPwVax/
    
    to make this work using DIVs this is amazing and I love it thanks for reading

--- index.php
+++ index.php
@@ -36,30 +36,24 @@
     </div>
 
     <div id="SI-Section" class="Container-SI">
-        <select size="19">
-          <option value="1" SELECTED>1</option>
-          <option value="2" >2</option>
-          <option value="3" >3</option>
-          <option value="4" >4</option>
-          <option value="5" >5</option>
-          <option value="6" >6</option>
-          <option value="7" >7</option>
-          <option value="8" >8</option>
-          <option value="9" >9</option>
-          <option value="10" >10</option>
-          <option value="11" >11</option>
-          <option value="12" >12</option>
-          <option value="13" >13</option>
-          <option value="14" >14</option>
-          <option value="15" >15</option>
-          <option value="16" >16</option>
-          <option value="17" >17</option>
-          <option value="18" >18</option>
-          <option value="19" >19</option>
-          <option value="20" >20</option>
-         </select>
+      <div id="events">
+        <div id="event-list">
+            <div class="content">
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+              <h2>Vendredi 17 octobre</h2>
+
+            </div>
+        </div>
     </div>
   </div>
+
+
 
 
 
