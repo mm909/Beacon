5c5c8859362de55f34e4828a30d1b462a458b3b8
mm909 <Bobar312@gmail.com>
2018-05-03 19:18:57 -0700

    Updating index.php
    Missed file in the last commit

--- index.php
+++ index.php
@@ -36,6 +36,7 @@
     </div>
 
     <div id="SI-Section" class="Container-SI">
+        <select size="19">
           <option value="1" SELECTED>1</option>
           <option value="2" >2</option>
           <option value="3" >3</option>
@@ -49,10 +50,15 @@
           <option value="11" >11</option>
           <option value="12" >12</option>
           <option value="13" >13</option>
+          <option value="14" >14</option>
+          <option value="15" >15</option>
+          <option value="16" >16</option>
+          <option value="17" >17</option>
+          <option value="18" >18</option>
+          <option value="19" >19</option>
+          <option value="20" >20</option>
          </select>
-      </div>
     </div>
-
   </div>
 
 
