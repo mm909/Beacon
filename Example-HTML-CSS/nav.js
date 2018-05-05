d5ee484a12c383b32bcea8403bf215f1e427df1b
mm909 <Bobar312@gmail.com>
2018-05-05 02:25:28 -0700

    Added AMAZING features
    With some jquery I was able to add in a feature that makes the map shrink based on your screen size.
    
    I just realized that a div element does not actaully show up yet but this is step one.

--- Example-HTML-CSS/nav.js
+++ Example-HTML-CSS/nav.js
@@ -1,7 +1,36 @@
 $( document ).ready(function() {
+
+    var Hnav = false;
+    var Vnav = false;
+
+
+    $('#threeLines').hover(function(){
+        if(!(Hnav || Vnav))
+        $(this).attr('src','Menu-Select.png');
+        }, function(){
+        if(!(Hnav || Vnav))
+        $(this).attr('src','Menu.png');
+    });
+
     $('#threeLines').on({
         'click': function(){
-            $('#threeLines').attr('src','Menu-Select.png');
+          if(Vnav || Hnav){
+            $(".Map").width('100%');
+            $(".Map").height('100%');
+            Vnav = false;
+            Hnav = false;
+            $(this).attr('src','Menu-Select.png');
+          } else if( $(".height").width()*1.2003841229193341 > 1500){
+              $(".Map").height('100%');
+              $(".Map").width('80%');
+              $(this).attr('src','Menu-Selected.png');
+              Vnav = true;
+          } else {
+              $(".Map").width('100%');
+              $(".Map").height('60%');
+              $(this).attr('src','Menu-Selected.png');
+              Hnav = true;
+          }
         }
     });
 });
