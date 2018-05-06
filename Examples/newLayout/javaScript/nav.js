bb18f14be07e39dd1048531669b88376694f0d73
mm909 <Bobar312@gmail.com>
2018-05-05 22:07:00 -0700

    New File Layout
    Added new file structure
    
    Next: add db stuff to the new layout

--- Examples/newLayout/javaScript/nav.js
+++ Examples/newLayout/javaScript/nav.js
@@ -1,16 +1,17 @@
 $( document ).ready(function() {
 
+    const ratio = 1.2003841229193341;
     var Hnav = false;
     var Vnav = false;
-    $("#container-tab-right").hide();
+    $("#container-tab").hide();
 
 
     $('#threeLines').hover(function(){
         if(!(Hnav || Vnav))
-        $(this).attr('src','Menu-Select.png');
+        $(this).attr('src',"images/Menu-Select.png");
         }, function(){
         if(!(Hnav || Vnav))
-        $(this).attr('src','Menu.png');
+        $(this).attr('src',"images/Menu.png");
     });
 
     $('#threeLines').on({
@@ -18,25 +19,25 @@ $( document ).ready(function() {
           if(Vnav || Hnav){
             $(".container-map").width('100%');
             $(".container-map").height('100%');
-            $("#container-tab-right").hide();
+            $("#container-tab").hide();
             Vnav = false;
             Hnav = false;
-            $(this).attr('src','Menu-Select.png');
-          } else if( $(".height").width()*1.2003841229193341 > 1500){
+            $(this).attr('src',"images/Menu-Select.png");
+          } else if( $(".height").width() * ratio > 1500){
               $(".container-map").height('100%');
               $(".container-map").width('80%');
-              $("#container-tab-right").width('20%');
-              $("#container-tab-right").height('100%');
-              $("#container-tab-right").show();
-              $(this).attr('src','Menu-Selected.png');
+              $("#container-tab").width('20%');
+              $("#container-tab").height('100%');
+              $("#container-tab").show();
+              $(this).attr('src',"images/Menu-Selected.png");
               Vnav = true;
           } else {
               $(".container-map").width('100%');
               $(".container-map").height('60%');
-              $("#container-tab-right").show();
-              $("#container-tab-right").width('100%');
-              $("#container-tab-right").height('40%');
-              $(this).attr('src','Menu-Selected.png');
+              $("#container-tab").show();
+              $("#container-tab").width('100%');
+              $("#container-tab").height('40%');
+              $(this).attr('src',"images/Menu-Selected.png");
               Hnav = true;
           }
         }
