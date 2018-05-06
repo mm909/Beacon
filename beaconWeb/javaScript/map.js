d51d4c1a43822b7f871ecb1d26918f4a8011a591
mm909 <Bobar312@gmail.com>
2018-05-06 03:16:20 -0700

    Added user location
    Kinda off -> Will look at it later

--- beaconWeb/javaScript/map.js
+++ beaconWeb/javaScript/map.js
@@ -16,7 +16,34 @@ function initMap(){
   // Make a new map with those options and place it in <div id="map"></div>
   map = new google.maps.Map(document.getElementById('map'), options);
 
+  infoWindow = new google.maps.InfoWindow;
 
+  if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var pos = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+           };
+
+           infoWindow.setPosition(pos);
+           infoWindow.setContent('Location found.');
+           infoWindow.open(map);
+           map.setCenter(pos);
+         }, function() {
+           handleLocationError(true, infoWindow, map.getCenter());
+         });
+       } else {
+         // Browser doesn't support Geolocation
+         handleLocationError(false, infoWindow, map.getCenter());
+       }
+
+       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+         infoWindow.setPosition(pos);
+         infoWindow.setContent(browserHasGeolocation ?
+                               'Error: The Geolocation service failed.' :
+                               'Error: Your browser doesn\'t support geolocation.');
+         infoWindow.open(map);
+       }
 
   // Get the list of markers (game stores) from our database
   var _markers = _receiveINFO();
@@ -45,4 +72,7 @@ function initMap(){
       map:map,
     });
   }
+
+
+
 }
