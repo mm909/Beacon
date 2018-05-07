200b8ce5acbf5c13526278edfd8b4da287423c51
mm909 <Bobar312@gmail.com>
2018-05-06 20:58:04 -0700

    Updated SI visuals

--- beaconWeb/javaScript/jQuery/si.js
+++ beaconWeb/javaScript/jQuery/si.js
@@ -6,8 +6,18 @@ function mapReady(){
   for(var i = 0; i < gameStore.length; i++){
     var storeName = gameStore[i].gameStoreName;
     var description = gameStore[i].description;
+    var logo = gameStore[i].logo;
+    console.log(logo)
     $( "#containerForSIItems" ).append(
-      '<div class="tab"><p>' + storeName + '</p></div>'
+      '<div id=store' + i + ' class="tab"></p></div>'
+    );
+
+    $( '#store'+i ).append(
+      '<p class="gameStoreName">'+ storeName + '</p>'
+    );
+
+    $( '#store'+i ).append(
+      '<img class="logo" src='+ logo + '>'
     );
   }
 
