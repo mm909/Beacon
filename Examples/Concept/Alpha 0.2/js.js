f71836b96c89953cf088b54ed7756217da226adf
mm909 <Bobar312@gmail.com>
2018-05-16 13:19:49 -0700

    Added JS functions

--- Examples/Concept/Alpha 0.2/js.js
+++ Examples/Concept/Alpha 0.2/js.js
@@ -1,3 +1,12 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 var nav = false;
 
@@ -9,5 +18,4 @@ function Nav() {
     document.getElementById("myNav").style.height = "100%";
     nav = true;
   }
-  console.log(nav)
 }
