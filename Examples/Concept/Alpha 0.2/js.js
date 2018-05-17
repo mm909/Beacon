4ede01d427fdd96cfd7c209638dc156daa92f8f8
mm909 <Bobar312@gmail.com>
2018-05-16 20:54:55 -0700

    Footer buttons work.
    Added footer buttons that work
    There is a know bug when they are clicked randomly but I am not worrying about that right now

--- Examples/Concept/Alpha 0.2/js.js
+++ Examples/Concept/Alpha 0.2/js.js
@@ -9,6 +9,7 @@ function getParameterByName(name, url) {
 }
 
 var nav = false;
+var si = false;
 
 function Nav() {
   if(nav){
@@ -18,4 +19,36 @@ function Nav() {
     document.getElementById("myNav").style.height = "100%";
     nav = true;
   }
+}
+
+function openSITable(name){
+  console.log(name);
+
+  if(si){
+    $('#map').css('height',$('#map').height()*2);
+    si = false;
+  } else {
+    $('#map').css('height',$('#map').height()/2);
+    si = true;
+  }
+
+  if(name == 'Stores' && si){
+    $('.tableStores').css('display','block');
+  }
+  if(name == 'Stores' && !si){
+    setTimeout(function() { $('.tableStores').css('display','none'); }, 500);
+  }
+  if(name == 'Beacons' && si){
+    $('.tableBeacons').css('display','block');
+  }
+  if(name == 'Beacons' && !si){
+    setTimeout(function() { $('.tableBeacons').css('display','none'); }, 500);
+  }
+  if(name == 'Events' && si){
+    $('.tableEvents').css('display','block');
+  }
+  if(name == 'Events' && !si){
+    setTimeout(function() { $('.tableEvents').css('display','none'); }, 500);
+  }
+
 }
