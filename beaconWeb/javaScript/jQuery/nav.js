63d3e33451819e3282ed07789cabff99e42338ef
mm909 <Bobar312@gmail.com>
2018-05-16 23:37:40 -0700

    Added nav file

---
+++ beaconWeb/javaScript/jQuery/nav.js
@@ -0,0 +1,10 @@
+var nav = false;
+function Nav() {
+  if(nav){
+    document.getElementById("myNav").style.height = "0%";
+    nav = false;
+  } else {
+    document.getElementById("myNav").style.height = "100%";
+    nav = true;
+  }
+}
--- beaconWeb/javaScript/jQuery/si.js
+++ beaconWeb/javaScript/jQuery/si.js
@@ -1,15 +1,4 @@
-var nav = false;
 var si = false;
-
-function Nav() {
-  if(nav){
-    document.getElementById("myNav").style.height = "0%";
-    nav = false;
-  } else {
-    document.getElementById("myNav").style.height = "100%";
-    nav = true;
-  }
-}
 
 function openSITable(name){
 
