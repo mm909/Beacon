6e09481be0b2a49ad168eb9dc5314533daf8f975
mm909 <Bobar312@gmail.com>
2018-05-18 17:51:37 -0700

    Changed CSS grids to flexbox for SI items

--- beaconWeb/javaScript/jQuery/si.js
+++ beaconWeb/javaScript/jQuery/si.js
@@ -11,21 +11,21 @@ function openSITable(name){
   }
 
   if(name == 'Stores' && si){
-    $('.tableStores').css('display','grid');
+    $('.tableStores').css('display','flex');
     $('.si').css('display','block');
   }
   if(name == 'Stores' && !si){
     setTimeout(function() { $('.tableStores').css('display','none'); $('.si').css('display','none'); }, 500);
   }
   if(name == 'Beacons' && si){
-    $('.tableBeacons').css('display','grid');
+    $('.tableBeacons').css('display','flex');
     $('.si').css('display','block');
   }
   if(name == 'Beacons' && !si){
     setTimeout(function() { $('.tableBeacons').css('display','none'); $('.si').css('display','none'); }, 500);
   }
   if(name == 'Events' && si){
-    $('.tableEvents').css('display','grid');
+    $('.tableEvents').css('display','flex');
     $('.si').css('display','block');
   }
   if(name == 'Events' && !si){
