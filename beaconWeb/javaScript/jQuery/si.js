48bd463554b9a81f8349007f8eeb0df0e4c17fc7
mm909 <Bobar312@gmail.com>
2018-05-18 17:16:44 -0700

    Starting work on adding CSS grids

--- beaconWeb/javaScript/jQuery/si.js
+++ beaconWeb/javaScript/jQuery/si.js
@@ -11,21 +11,21 @@ function openSITable(name){
   }
 
   if(name == 'Stores' && si){
-    $('.tableStores').css('display','block');
+    $('.tableStores').css('display','grid');
     $('.si').css('display','block');
   }
   if(name == 'Stores' && !si){
     setTimeout(function() { $('.tableStores').css('display','none'); $('.si').css('display','none'); }, 500);
   }
   if(name == 'Beacons' && si){
-    $('.tableBeacons').css('display','block');
+    $('.tableBeacons').css('display','grid');
     $('.si').css('display','block');
   }
   if(name == 'Beacons' && !si){
     setTimeout(function() { $('.tableBeacons').css('display','none'); $('.si').css('display','none'); }, 500);
   }
   if(name == 'Events' && si){
-    $('.tableEvents').css('display','block');
+    $('.tableEvents').css('display','grid');
     $('.si').css('display','block');
   }
   if(name == 'Events' && !si){
