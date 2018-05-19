d8735bc0ff8e3d8088d83afc063454cb781b054d
mm909 <Bobar312@gmail.com>
2018-05-19 01:12:18 -0700

    Phone + functions
    Created a function for redundent code
    And updated the phone number

--- beaconWeb/javaScript/jQuery/buildSI.js
+++ beaconWeb/javaScript/jQuery/buildSI.js
@@ -85,98 +85,8 @@ function buildSI(){
           $( "#storeSorter" ).append($siStoreSorterOption);
 
       bubbleSort(gameStore, $( "#storeSorter" ).val())
+      popluateSICards();
 
-
-      /*
-      <div class="siItem">
-        <div class="si-Store-Header">
-          <a class="storeName" href="#">Darkside Games</a>
-          <hr>
-          <a class="directions" href="#">Directions</a>
-          <p class="distance">X miles</p>
-        </div>
-        <hr class="headerBreak">
-        <div class="siButtons">
-          <div class="btn-group btn-group-justified">
-            <a href="#" onclick="" class="btn btn-dark">Players: x</a>
-            <a href="#" onclick="" class="btn btn-dark">Beacons: x</a>
-            <a href="#" onclick="" class="btn btn-dark">Events: x</a>
-          </div>
-        </div>
-        <hr class="buttonBreak">
-        <p class="phone">702-540-4190</p>
-      </div>
-      */
-
-      for(var i = 0; i < gameStore.length; i++){
-
-        /* Get address for nav link */
-        var address1    = gameStore[i].street;
-        var address2    = gameStore[i].city;
-            address2   +=  ", " + gameStore[i].state;
-            address2   += " " + gameStore[i].zip;
-        var navAddress  = "https://www.google.com/maps/dir/?api=1&destination=" + address1 + address2;
-
-        /* Create the card */
-        $siStoreCard = $( "<div class='siItem' id='exampleGameStoreCard" + i + "' ></div>" )
-        $( ".tableStores" ).append($siStoreCard);
-        //--
-
-            /* Create the header part */
-            $siStoreHeader = $( "<div id='exampleGameStoreHeader" + i + "' class='si-Store-Header'></div>" )
-            $( "#exampleGameStoreCard" + i ).append($siStoreHeader)
-            //--
-
-                /* Create Store Name */
-                $siStoreName = $( "<a id='exampleGameStoreName" + i + "' class='storeName' href='#'>" + gameStore[i].gameStoreName + "</a>")
-                $( "#exampleGameStoreHeader" + i ).append($siStoreName)
-
-                /* Create Break */
-                $siStoreBreak = $( "<hr>")
-                $( "#exampleGameStoreHeader" + i ).append($siStoreBreak)
-
-                /* Create Directions Link */
-                $siStoreDirections = $( "<a class='directions' target='_blank' href='" + navAddress + "'>Directions</a>")
-                $( "#exampleGameStoreHeader" + i ).append($siStoreDirections)
-
-                /* Create Distance */
-                $siStoreDistance = $( "<p class='distance'>" + gameStore[i].distance + " miles</p>")
-                $( "#exampleGameStoreHeader" + i ).append($siStoreDistance)
-
-            /* Create Header Break */
-            $siStoreHaederBreak = $( "<hr class='headerBreak'>")
-            $( "#exampleGameStoreCard" + i ).append($siStoreHaederBreak)
-
-            /* Create the buttons part */
-            $siStoreButtons = $( "<div id='exampleGameStoreButtons" + i + "' class='siButtons'>" )
-            $( "#exampleGameStoreCard" + i ).append($siStoreButtons)
-            //--
-
-                /* Create the buttons container */
-                $siStoreButtonContainer = $( "<div id='exampleGameStoreButtonContainer" + i + "' class='btn-group btn-group-justified'>" )
-                $( "#exampleGameStoreButtons" + i ).append($siStoreButtonContainer)
-                //--
-
-                    /* Create Button*/
-                    $siStoreButton1 = $( "<a href='#' onclick='' class='btn btn-dark'>Players: " + gameStore[i].playersTotal + "</a>" )
-                    $( "#exampleGameStoreButtonContainer" + i ).append($siStoreButton1)
-
-                    /* Create Button*/
-                    $siStoreButton2 = $( "<a href='#' onclick='' class='btn btn-dark'>Beacons: " + gameStore[i].beaconsTotal + "</a>" )
-                    $( "#exampleGameStoreButtonContainer" + i ).append($siStoreButton2)
-
-                    /* Create Button*/
-                    $siStoreButton3 = $( "<a href='#' onclick='' class='btn btn-dark'>Events: " + gameStore[i].eventsTotal + "</a>" )
-                    $( "#exampleGameStoreButtonContainer" + i ).append($siStoreButton3)
-
-            /* Create Button Break */
-            $siStoreButtonBreak = $( "<hr class='buttonBreak'>")
-            $( "#exampleGameStoreCard" + i ).append($siStoreButtonBreak)
-
-            /* Create Phone Number */
-            $siStorePhone = $( "<p class='phone'>702-540-4190</p>")
-            $( "#exampleGameStoreCard" + i ).append($siStorePhone)
-      }
   });
 }
 
@@ -232,11 +142,12 @@ function rearangeSI(sortVal) {
         $( "#storeSorter" ).append($siStoreSorterOption);
       }
 
-
-
       bubbleSort(gameStore, sortVal)
-      console.log("changed and sorted " + sortVal)
+      popluateSICards();
 
+}
+
+function popluateSICards(){
   /*
   <div class="siItem">
     <div class="si-Store-Header">
@@ -324,7 +235,7 @@ function rearangeSI(sortVal) {
         $( "#exampleGameStoreCard" + i ).append($siStoreButtonBreak)
 
         /* Create Phone Number */
-        $siStorePhone = $( "<p class='phone'>702-540-4190</p>")
+        $siStorePhone = $( "<p class='phone'>" + phoneNumberParser(gameStore[i].phone) + "</p>")
         $( "#exampleGameStoreCard" + i ).append($siStorePhone)
   }
 }
