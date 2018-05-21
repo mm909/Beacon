393e7b7d9ebe2580e79cc50bb1285c613e3501c0
mm909 <Bobar312@gmail.com>
2018-05-20 21:25:22 -0700

    Adding back in old function

--- beaconWeb/javaScript/shared.js
+++ beaconWeb/javaScript/shared.js
@@ -140,6 +140,17 @@ function bubbleSort(a,sort)
     }
 }
 
+function getDistance(user, store){
+  // Use the google api to get the distance is meters
+  var dist = user.distanceFrom(store);
+  // Convert that to miles and round to the tenth
+  dist = dist/1000 * 0.621371;
+  dist *= 10;
+  dist = Math.floor(dist);
+  dist /= 10;
+  return dist;
+}
+
 function phoneNumberParser(number){
   return '(' + number[0] + number[1] + number[2] + ') ' + number[3] + number[4] + number[5] + '-' + number[6] + number[7] + number[8] + number[9];
 }
