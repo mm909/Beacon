53843c82f328cfbaf4cb0b1bb07640e6212afa23
mm909 <Bobar312@gmail.com>
2018-05-22 15:03:37 -0700

    Added in timers from database

--- Examples/Concept/BeaconTimer/timer.js
+++ Examples/Concept/BeaconTimer/timer.js
@@ -1,20 +1,28 @@
+/*
 date = [[2018, 05, 22, 14, 6, 00], [2018, 05, 22, 12, 34, 04], [2018, 05, 22, 13, 53, 04], [2018, 05, 22, 12, 03, 05],
         [2018, 05, 22, 11, 03, 09], [2018, 05, 22, 15, 32, 26], [2018, 05, 22, 13, 08, 20], [2018, 05, 22, 12, 10, 00]]
-        console.table(date)
+*/
+
+var date = [];
 
 window.setInterval(function(){
   initTimes();
 }, 1000);
 
 $(document).ready(function(){
+  parseTimes();
   initTimes();
 });
-
 
 // YYYY-MM-DD HH:MM:SS
 // {YYYY, MM, DD, HH, MM, SS}
 function parseTimes() {
-
+    for(var i = 0; i < beacons.length; i++){
+      str = beacons[i].dateTimeCreated;
+      date[i] = [ parseInt(str.substring(0,4)), parseInt(str.substring(5,7)),
+                  parseInt(str.substring(8,10)), parseInt(str.substring(11,13)),
+                  parseInt(str.substring(14,16)), parseInt(str.substring(17,19))]
+    }
 }
 
 // {YYYY, MM, DD, HH, MM, SS}
